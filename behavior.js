a332ce32a4899e0614300e62625627eebc51d326
juliana <choisjuliana@gmail.com>
2020-03-23 01:08:57 -0500

    pages added

---
+++ behavior.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    /* alert('Hello!'); */
+
+    document.getElementById("smart_thumbnail");
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+        // write here
+        alert("I saw you click!");
+      });
+
+});
